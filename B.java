3fa513028fcd5f2bfe21a81734d9524a4926b981
galina <dvizenie77@gmail.com>
2022-10-21 21:03:03 +0200

    test

--- B.java
+++ B.java
@@ -1,5 +1,26 @@
-public class B {
+package com.example.backend.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+djflkajdlfkj
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "sights")
+public class Sight {
 
+    @Id
+    private String id;
+
+    private String name;
+    private String image;
+    private String adresse;
+    private String webseite;
+    private String uhrzeit;
+    private String beschreibung;
+    private String lokation;
 
 }
